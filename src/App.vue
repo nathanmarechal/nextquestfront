3c132f55687bd1584c42cd170073b00ec2cd71da
Nathan MARECHAL <nathan.marechal@edu.univ-fcomte.fr>
2025-05-01 23:51:51 +0200

    NavBegin

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="nav">
+    <NavBar/>
+    <h1>Welcome to Your Vue.js App</h1>
   </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app flat color="#180342" class="px-4">
+    <v-container class="d-flex align-center justify-space-between" fluid>
+
+      <v-img src="@/assets/NextQuest.png" max-height="40" contain class="mr-6" />
+
+      <v-row class="align-center" no-gutters>
+        <v-img src="@/assets/Accueil.png" max-height="40" contain class="mx-3" />
+        <v-img src="@/assets/Menu.png" max-height="40" contain class="mx-3" />
+        <v-img src="@/assets/Recherche.png" max-height="40" contain class="mx-3" />
+        <v-img src="@/assets/Connexion.png" max-height="40" contain class="mx-3" />
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.v-img {
+  cursor: pointer;
+  transition: transform 0.2s ease;
+}
+.v-img:hover {
+  transform: scale(1.05);
+}
+</style>
