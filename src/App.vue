b89444bf3351c3edc9308ef8d6d3628d884db688
Nathan MARECHAL <nathan.marechal@edu.univ-fcomte.fr>
2025-05-03 00:30:01 +0200

    footerv1

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <div class="content">
       <router-view></router-view>
     </div>
+    <FooterComponent></FooterComponent>
     <!-- <router-link to="/"></router-link> -->
     <!--<div class="content">-->
       <!--<router-view></router-view>-->
@@ -20,7 +21,9 @@
 export default {
   name: 'App',
   components: {
-    NavBar: () => import('./components/NavBar.vue')
+    NavBar: () => import('./components/NavBar.vue'),
+    // eslint-disable-next-line vue/no-unused-components
+    FooterComponent: () => import('./components/FooterComponent.vue')
   },
 
   data: () => ({
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer class="site-footer">
+    <div class="footer-content">
+      <img src="@/assets/NextQuest.png" alt="Logo" class="footer-logo" />
+      <p class="footer-text">© 2025 Nathan MARECHAL. Tous droits réservés.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    logo: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.site-footer {
+  width: 100%;
+  padding: 24px 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(to bottom, #180342, #000000);
+}
+
+.footer-content {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.footer-logo {
+  height: 72px;
+}
+
+.footer-text {
+  color: #ccc;
+  font-size: 14px;
+  text-align: right;
+}
+</style>
