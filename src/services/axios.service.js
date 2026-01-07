aba4fcc528cb28eacc61118257bf11a580db6621
Nathan MARECHAL <nathan.marechal@edu.univ-fcomte.fr>
2026-01-07 23:06:15 +0100

    env

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from '@/store'
 
 const axiosAgent = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: (import.meta?.env?.VITE_API_BASE_URL ?? process.env.VUE_APP_API_BASE_URL ?? '/api'),
   withCredentials: true
 })
 
